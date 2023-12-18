a31014ab91b6f300a3e717c55212e9d44853b5e1
briksdaal <102250834+briksdaal@users.noreply.github.com>
2023-12-18 18:59:06 +0200

    Use ejs template engine

--- index.js
+++ index.js
@@ -1,22 +1,36 @@
 const express = require('express');
 
 const app = express();
-const viewsDir = __dirname + '/views';
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.sendFile(viewsDir + '/index.html');
+  res.render('page.ejs', {
+    title: 'Home',
+    description: 'This is the home page. The main page. The pagest of pages.',
+  });
 });
 
 app.get('/about', (req, res) => {
-  res.sendFile(viewsDir + '/about.html');
+  res.render('page.ejs', {
+    title: 'About Me',
+    description: 'I am a person of interest.',
+  });
 });
 
 app.get('/contact-me', (req, res) => {
-  res.sendFile(viewsDir + '/contact-me.html');
+  res.render('page.ejs', {
+    title: 'Contact Me',
+    description:
+      "You'd expect my contact details to show up here. I guess I should've put them up.",
+  });
 });
 
 app.use((req, res) => {
-  res.sendFile(viewsDir + '/404.html');
+  res.status(404).render('page.ejs', {
+    title: '404 Not Found',
+    description: 'Nothing here, eh?',
+  });
 });
 
 app.listen(8080);
